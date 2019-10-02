b616354c0fdd32a35589cf70d9443ab1f69b11a6
Austin <akc257@nau.edu>
2019-10-02 16:43:33 -0700

    fix

--- charRecognition.py
+++ charRecognition.py
@@ -1,10 +1,9 @@
-# Import `tensorflow`
-import tensorflow as tf
-
 # Import numpy for array usage
 import numpy as np
 
 # Importing test and training data
-train_array = np.loadtxt("~/Documents/GitHub/CS599_P1/zip.train.txt")
+train_array = np.loadtxt("/Users/austincorum/Documents/GitHub/CS599_P1/zip.test")
 # Change from array to a data frame for 2-dimmentional data structure
     # For abtracting the data into rows and columns
+train_frame = np.DataFrame(train_array)
+train_frame.shape
