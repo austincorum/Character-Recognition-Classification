42f6d792c59bd1d152ae79a6d5c1a764cba43fab
Austin <akc257@nau.edu>
2019-10-02 16:49:04 -0700

    pandas import

--- charRecognition.py
+++ charRecognition.py
@@ -1,9 +1,11 @@
 # Import numpy for array usage
 import numpy as np
+# Import pandas for Data frame
+import pandas as pd
 
 # Importing test and training data
 train_array = np.loadtxt("/Users/austincorum/Documents/GitHub/CS599_P1/zip.test")
 # Change from array to a data frame for 2-dimmentional data structure
     # For abtracting the data into rows and columns
-train_frame = np.DataFrame(train_array)
+train_frame = pd.DataFrame(train_array)
 train_frame.shape
