95790c0c2e55de2e27ed12014b414b621b5c73b5
Austin <akc257@nau.edu>
2019-10-02 15:35:13 -0700

    imports for tensor flow and net-1

---
+++ charRecognition.py
@@ -0,0 +1,6 @@
+# Import `tensorflow`
+import tensorflow as tf
+# imports for net-1
+from sklearn.linear_model import LogisticRegression
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
