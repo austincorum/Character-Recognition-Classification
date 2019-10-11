f5a0a38de7292566bdf066d712959fd02567546c
Austin <akc257@nau.edu>
2019-10-11 16:40:15 -0700

    minor updates to makefile and charRec file

--- charRecognition.py
+++ charRecognition.py
@@ -12,6 +12,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+############## LOADING DATA ##################
 # currently importing test data from external source to understand the process
 
 _URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
@@ -46,8 +48,48 @@ print("--")
 print("Total training images:", total_train)
 print("Total validation images:", total_val)
 
+
+# ###################### LOAD DATA #############################
+# # Load training data
+# train_array = np.loadtxt("/Users/austincorum/Documents/GitHub/CS599_P1/zip.train")
+#
+#     # Change from array to a data frame for 2-dimmentional data structure
+#         # For abtracting the data into rows and columns
+# train_data = pd.DataFrame(train_array)
+#
+#     # View the data shape
+# print(train_data.shape)
+#     # output is (7291, 257)
+#
+# # Load testing data
+# test_array = np.loadtxt("/Users/austincorum/Documents/GitHub/CS599_P1/zip.test")
+#     # Change from array to a data frame for 2-dimmentional data structure
+#     # for abtracting the data into rows and columns
+# test_data = pd.DataFrame(test_array)
+#     # View the data shape
+# print(test_data.shape)
+#     # Output is (2007, 257)
+#
+# # TRAINING DATA classified from 0 to 9
+# for i in range(0,9):
+#     # Look through data rows for images
+#     train_rows = train_array[i][1:]
+#     # Returns a matrix from an array-like object
+#     elements = np.matrix(train_rows)
+#     # Read the elements using this index order
+#     elements = elements.reshape(16,16)
+#     # Creates new figure width and height
+#     plt.figure(figsize=(10,10))
+#     # Creates a figure and a grid of subplots
+#     plt.subplot(3,3,i+1)
+#     # An image with scalar data visualized using a colormap
+#     plt.imshow(elements)
+#     # To view each image in matplotlib
+#     plt.show()
+#         # Exit for loop
+
 batch_size = 128
-epochs = 15
+epochs = 5
 IMG_HEIGHT = 150
 IMG_WIDTH = 150
 
@@ -125,42 +167,3 @@ plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
-
-# ###################### LOAD DATA #############################
-# # Load training data
-# train_array = np.loadtxt("/Users/austincorum/Documents/GitHub/CS599_P1/zip.train")
-#
-#     # Change from array to a data frame for 2-dimmentional data structure
-#         # For abtracting the data into rows and columns
-# train_data = pd.DataFrame(train_array)
-#
-#     # View the data shape
-# print(train_data.shape)
-#     # output is (7291, 257)
-#
-# # Load testing data
-# test_array = np.loadtxt("/Users/austincorum/Documents/GitHub/CS599_P1/zip.test")
-#     # Change from array to a data frame for 2-dimmentional data structure
-#     # for abtracting the data into rows and columns
-# test_data = pd.DataFrame(test_array)
-#     # View the data shape
-# print(test_data.shape)
-#     # Output is (2007, 257)
-#
-# # TRAINING DATA classified from 0 to 9
-# for i in range(0,9):
-#     # Look through data rows for images
-#     train_rows = train_array[i][1:]
-#     # Returns a matrix from an array-like object
-#     elements = np.matrix(train_rows)
-#     # Read the elements using this index order
-#     elements = elements.reshape(16,16)
-#     # Creates new figure width and height
-#     plt.figure(figsize=(10,10))
-#     # Creates a figure and a grid of subplots
-#     plt.subplot(3,3,i+1)
-#     # An image with scalar data visualized using a colormap
-#     plt.imshow(elements)
-#     # To view each image in matplotlib
-#     plt.show()
-#         # Exit for loop
--- makefile
+++ makefile
@@ -1,7 +1,12 @@
 # makefile
-# install numpy
-# install pandas
-# install matplotlib.pyplot
+# pip install numpy
+# pip install pandas
+# pip install matplotlib.pyplot
+# pip install pillow
+# pip install tensorflow
+# pip install scipy
+
+
 # don't install packages if previously installed
 
 python charRecognition.py
