53fa42a4a385f48eb30390e5a22cc16fa9a7ca61
Austin <akc257@nau.edu>
2019-10-17 11:55:18 -0700

    makefile and charRecUpdated

--- charRecognition.py
+++ charRecognition.py
@@ -14,44 +14,47 @@ import matplotlib.pyplot as plt
 
 
 ############## LOADING DATA ##################
-# currently importing test data from external source to understand the process
-
-_URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
-
-path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
-
-PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
-
-train_dir = os.path.join(PATH, 'train')
-validation_dir = os.path.join(PATH, 'validation')
-
-train_cats_dir = os.path.join(train_dir, 'cats')  # directory with our training cat pictures
-train_dogs_dir = os.path.join(train_dir, 'dogs')  # directory with our training dog pictures
-validation_cats_dir = os.path.join(validation_dir, 'cats')  # directory with our validation cat pictures
-validation_dogs_dir = os.path.join(validation_dir, 'dogs')  # directory with our validation dog pictures
-
-num_cats_tr = len(os.listdir(train_cats_dir))
-num_dogs_tr = len(os.listdir(train_dogs_dir))
-
-num_cats_val = len(os.listdir(validation_cats_dir))
-num_dogs_val = len(os.listdir(validation_dogs_dir))
-
-total_train = num_cats_tr + num_dogs_tr
-total_val = num_cats_val + num_dogs_val
-
-print('total training cat images:', num_cats_tr)
-print('total training dog images:', num_dogs_tr)
-
-print('total validation cat images:', num_cats_val)
-print('total validation dog images:', num_dogs_val)
-print("--")
-print("Total training images:", total_train)
-print("Total validation images:", total_val)
-
+# # currently importing test data from external source to understand the process
+#
+# _URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
+#
+# path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
+#
+# PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
+#
+# train_dir = os.path.join(PATH, 'train')
+#
+# validation_dir = os.path.join(PATH, 'validation')
+#
+# train_cats_dir = os.path.join(train_dir, 'cats')  # directory with our training cat pictures
+# train_dogs_dir = os.path.join(train_dir, 'dogs')  # directory with our training dog pictures
+# validation_cats_dir = os.path.join(validation_dir, 'cats')  # directory with our validation cat pictures
+# validation_dogs_dir = os.path.join(validation_dir, 'dogs')  # directory with our validation dog pictures
+#
+# num_cats_tr = len(os.listdir(train_cats_dir))
+# num_dogs_tr = len(os.listdir(train_dogs_dir))
+#
+# num_cats_val = len(os.listdir(validation_cats_dir))
+# num_dogs_val = len(os.listdir(validation_dogs_dir))
+#
+# total_train = num_cats_tr + num_dogs_tr
+# total_val = num_cats_val + num_dogs_val
+#
+# print('total training cat images:', num_cats_tr)
+# print('total training dog images:', num_dogs_tr)
+#
+# print('total validation cat images:', num_cats_val)
+# print('total validation dog images:', num_dogs_val)
+# print("--")
+# print("Total training images:", total_train)
+# print("Total validation images:", total_val)
 
 ###################### LOAD DATA #############################
+print("\n\n--------------- MY DATA -----------------------\n")
 # Load training data
 train_array = np.loadtxt("/Users/austincorum/Documents/GitHub/CS599_P1/zip.train")
+train_levels = train_array[0:1000][0]
+train_features = train_array[0:1000][1:257]
 
     # Change from array to a data frame for 2-dimmentional data structure
         # For abtracting the data into rows and columns
@@ -70,6 +73,7 @@ test_data = pd.DataFrame(test_array)
 print(test_data.shape)
     # Output is (2007, 257)
 
+# visualize data
 # TRAINING DATA classified from 0 to 9
 for i in range(0,9):
     # Look through data rows for images
@@ -85,41 +89,43 @@ for i in range(0,9):
     # An image with scalar data visualized using a colormap
     plt.imshow(elements)
     # To view each image in matplotlib
-    plt.show()
+    #plt.show()
         # Exit for loop
 
 batch_size = 128
-epochs = 15
-IMG_HEIGHT = 150
-IMG_WIDTH = 150
+# figure had 30 training epochs
+epochs = 30
+# 16 x 16 greyscale images
+IMG_HEIGHT = 16
+IMG_WIDTH = 16
 
-train_image_generator = ImageDataGenerator(rescale=1./255) # Generator for our training data
-validation_image_generator = ImageDataGenerator(rescale=1./255) # Generator for our validation data
+# train_image_generator = ImageDataGenerator(rescale=1./255) # Generator for our training data
+# validation_image_generator = ImageDataGenerator(rescale=1./255) # Generator for our validation data
+#
+# train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
+#                                                            directory=train_dir,
+#                                                            shuffle=True,
+#                                                            target_size=(IMG_HEIGHT, IMG_WIDTH),
+#                                                            class_mode='binary')
+#
+# val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
+#                                                               directory=validation_dir,
+#                                                               target_size=(IMG_HEIGHT, IMG_WIDTH),
+#                                                               class_mode='binary')
+#
+# sample_training_images, _ = next(train_data_gen)
+#
+# # This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
+# def plotImages(images_arr):
+#     fig, axes = plt.subplots(1, 5, figsize=(20,20))
+#     axes = axes.flatten()
+#     for img, ax in zip( images_arr, axes):
+#         ax.imshow(img)
+#         ax.axis('off')
+#     plt.tight_layout()
+#     # plt.show()
 
-train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
-                                                           directory=train_dir,
-                                                           shuffle=True,
-                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                           class_mode='binary')
-
-val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
-                                                              directory=validation_dir,
-                                                              target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                              class_mode='binary')
-
-sample_training_images, _ = next(train_data_gen)
-
-# This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
-def plotImages(images_arr):
-    fig, axes = plt.subplots(1, 5, figsize=(20,20))
-    axes = axes.flatten()
-    for img, ax in zip( images_arr, axes):
-        ax.imshow(img)
-        ax.axis('off')
-    plt.tight_layout()
-    # plt.show()
-
-plotImages(sample_training_images[:5])
+# plotImages(sample_training_images[:5])
 
 model = Sequential([
     Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
--- makefile
+++ makefile
@@ -1,15 +1,8 @@
 # makefile
 # ran on MAC OSX 10.15
-# ran using python 2.6
-
-# pip install numpy
-# pip install pandas
-# pip install matplotlib.pyplot
-# pip install pillow
-# pip install tensorflow
-# pip install scipy
-
+# install python 2.6
 
 # don't install packages if previously installed
 
+pip install -r make.txt
 python charRecognition.py
