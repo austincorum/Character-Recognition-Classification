c540386291fd5d72e17f2d9eed63a575f8994586
Austin <akc257@nau.edu>
2019-10-18 22:38:21 -0700

    net-1 implemented, figure is produced but not the same

--- charRecognition.py
+++ charRecognition.py
@@ -4,127 +4,132 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, LocallyConnected2D
+from tensorflow.keras.utils import to_categorical
 
-import pandas as pd
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-############## LOADING DATA ##################
-# # currently importing test data from external source to understand the process
-#
-# _URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
-#
-# path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
-#
-# PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
-#
-# train_dir = os.path.join(PATH, 'train')
-#
-# validation_dir = os.path.join(PATH, 'validation')
-#
-# train_cats_dir = os.path.join(train_dir, 'cats')  # directory with our training cat pictures
-# train_dogs_dir = os.path.join(train_dir, 'dogs')  # directory with our training dog pictures
-# validation_cats_dir = os.path.join(validation_dir, 'cats')  # directory with our validation cat pictures
-# validation_dogs_dir = os.path.join(validation_dir, 'dogs')  # directory with our validation dog pictures
-#
-# num_cats_tr = len(os.listdir(train_cats_dir))
-# num_dogs_tr = len(os.listdir(train_dogs_dir))
-#
-# num_cats_val = len(os.listdir(validation_cats_dir))
-# num_dogs_val = len(os.listdir(validation_dogs_dir))
-#
-# total_train = num_cats_tr + num_dogs_tr
-# total_val = num_cats_val + num_dogs_val
-#
-# print('total training cat images:', num_cats_tr)
-# print('total training dog images:', num_dogs_tr)
-#
-# print('total validation cat images:', num_cats_val)
-# print('total validation dog images:', num_dogs_val)
-# print("--")
-# print("Total training images:", total_train)
-# print("Total validation images:", total_val)
-
-###################### LOAD DATA #############################
-print("\n\n--------------- MY DATA -----------------------\n")
-# Load training data
-train_array = np.loadtxt("/Users/austincorum/Documents/GitHub/CS599_P1/zip.train")
-train_levels = train_array[0:1000][0]
-print(train_levels)
-train_features = train_array[0:1000][1:257]
-
-    # Change from array to a data frame for 2-dimmentional data structure
-        # For abtracting the data into rows and columns
-train_data = pd.DataFrame(train_array)
-
-    # View the data shape
-print(train_data.shape)
-    # output is (7291, 257)
-
-# Load testing data
-test_array = np.loadtxt("/Users/austincorum/Documents/GitHub/CS599_P1/zip.test")
-    # Change from array to a data frame for 2-dimmentional data structure
-    # for abtracting the data into rows and columns
-test_data = pd.DataFrame(test_array)
-    # View the data shape
-print(test_data.shape)
-    # Output is (2007, 257)
+import os
 
 
-batch_size = 128
-# figure had 30 training epochs
-epochs = 30
-# 16 x 16 greyscale images
-IMG_HEIGHT = 16
-IMG_WIDTH = 16
+def main():
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    ###################### LOADING DATA INTO ARAYS #############################
+        # Load training data into an array
+    train_array = np.loadtxt("/Users/austincorum/Documents/GitHub/CS599_P1/zip.train")
+        # extracting the levels from the training array
+    train_levels_data = train_array[0:7291, 0:1]
+        # convert the class vector to binary class matrix
+            # as many columns as there are classes
+    train_levels = to_categorical(train_levels_data)
+        # extracting the features from the training array
+    train_features = train_array[0:7291, 1:257]
+        # Get total number of training data
+    total_train = len(train_features)
 
-model = Sequential([
-    Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
-    MaxPooling2D(),
-    Conv2D(32, 3, padding='same', activation='relu'),
-    MaxPooling2D(),
-    Conv2D(64, 3, padding='same', activation='relu'),
-    MaxPooling2D(),
-    Flatten(),
-    Dense(512, activation='relu'),
-    Dense(1, activation='sigmoid')
-])
+        # Load testing data into an array
+    test_array = np.loadtxt("/Users/austincorum/Documents/GitHub/CS599_P1/zip.test")
+        # extract test levels from test_array, for a sample of data
+    test_levels = test_array[0:2007, 0:1]
+        # extract teat features from test_array, for a sample of data
+    test_features = test_array[0:2007, 1:257]
 
-model.compile(optimizer='adam',
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-model.summary()
+        # Get total number of test data
+    total_test = len(test_features)
+        # total size of all data
+    total_val = total_test + total_train
 
-history = model.fit_generator(
-    train_data_gen,
-    steps_per_epoch=total_train // batch_size,
-    epochs=epochs,
-    validation_data=val_data_gen,
-    validation_steps=total_val // batch_size
-)
+        # View the data shape
+    # print(test_features.shape)
+        # Output is (2007, 257)
 
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
+    # this size is not given in the book
+    batch_size = 256
+    # figure had 30 training epochs
+    start_epoch = 0
+    num_epochs = 30
+    interval = 1
+    # 16 x 16 greyscale images
+    IMG_HEIGHT = 16
+    IMG_WIDTH = 16
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+        # Net-2 Archetecture (two layers dense)
+    model_two = Sequential([
+                Dense(12, input_dim=256, activation='sigmoid'),
+                Dense(10, input_dim=256,  activation='sigmoid')
+    ])
+        # Net-3 Archetecture (two local and one Dense)
+    model_three = Sequential([
+                LocallyConnected2D(64, (3,3), input_shape=(16, 16,1), activation='sigmoid'),
+                LocallyConnected2D(16, (5,5), activation='sigmoid'),
+                Flatten(),
+                Dense(10, activation='sigmoid')
+    ])
+        # Net-4 Archetecture (conv2d locally connected, and dense last layer)
+    model_four = Sequential([
+                Conv2D(128,(3,3), input_shape=(16,16,1), activation='sigmoid'),
+                LocallyConnected2D(16, (5,5), activation='sigmoid'),
+                Flatten(),
+                Dense(10, activation='sigmoid')
+    ])
+        # Net-5 Archetecture (conv2d, dense layer)
+    model_five = Sequential([
+                Conv2D(128,(3,3), input_shape=(16,16,1), activation='sigmoid'),
+                LocallyConnected2D(16, (5,5), activation='sigmoid'),
+                Flatten(),
+                Dense(10, activation='sigmoid')
+    ])
 
-epochs_range = range(epochs)
+        # currently only trying toget the network one correct percent on test data
+            # archetecture seems tight but isnt giving vallues shown in the figure
+    net1_correct = net_one(train_levels, train_features, test_levels, test_features, num_epochs, batch_size)
 
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+    graph_properties = np.arange(start_epoch, num_epochs, interval)
+    plt.figure(figsize=(8, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(graph_properties, net1_correct, label='Net-1')
+    plt.xlabel("Training Epochs")
+    plt.ylabel("% Correct on Test Data")
+    plt.legend(loc='lower right')
+    plt.show()
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
+
+# net-1 function
+    # takes in train and test features/levels, as well as num_epochs and the batch size
+def net_one(train_levels, train_features, test_levels, test_features, num_epochs, batch_size):
+    total_accuracy = []
+        # Net-1 Archetecture
+    model_one = Sequential([
+            Dense(10, input_dim=256, activation='sigmoid')
+    ])
+        # sum of squared error loss
+    model_one.compile(optimizer='sgd',
+                  loss=tf.losses.MeanSquaredError(),
+                  metrics=['accuracy'])
+
+    for epoch in range(num_epochs):
+            # fit the model for all epochs
+        model_one.fit(
+            train_features,
+            train_levels,
+            batch_size=batch_size,
+            epochs=epoch
+        )
+            # evaluate the model
+        loss, accuracy = model_one.evaluate(train_features, train_levels, batch_size=batch_size)
+        print('test loss, test acc:', loss, " ", accuracy)
+        predicted_classes = model_one.predict_classes(test_features)
+        correct_predictions = 0
+        for x in range(len(predicted_classes)):
+            if predicted_classes[x] == test_levels[x]:
+                correct_predictions += 1
+            else:
+                pass
+        accuracy = 100 * (correct_predictions/len(predicted_classes))
+        total_accuracy.append(accuracy)
+    return total_accuracy
+
+if __name__ == '__main__':
+    main()
